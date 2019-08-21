9dde7c39d6da056682cc4f8e3bbf5bb1bdb56432
Александр Сташинский <alexanderstashinski@gmail.com>
2019-08-21 13:08:29 +0300

    Добавьте файлы проекта.

---
+++ GameStore.Domain/Abstract/IGameRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameStore.Domain.Entities;
+using GameStore.Domain.Abstract;
+
+
+namespace GameStore.Domain.Abstract
+{
+    public interface IGameRepository
+    {
+        IEnumerable<Game> Games { get; }
+    }
+}
---
+++ GameStore.Domain/Concrete/EFGameRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using GameStore.Domain.Entities;
+using GameStore.Domain.Abstract;
+
+namespace GameStore.Domain.Concrete
+{
+    public class EFGameRepository : IGameRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Game> Games
+        {
+            get { return context.Games; }
+        }
+    }
+}
